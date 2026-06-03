e719a7f6587897dd018b846b87f484bb3119d11a
Luiz Otávio de Almeida <luizotavioodealemida@gmail.com>
2026-06-03 11:13:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,32 +10,22 @@ def main() -> None:
     ARQUIVO_CSV = "dados/pacientes_raw.csv"
     BANCO_NOME = "triagem_database.db"
     QTD_REGISTROS = 50
-    
-    # ---------------------------------------------------------
-    # Etapa 1: Gerar os dados sujos (Simulação da Entrada)
-    # ---------------------------------------------------------
+  
     print("\n[Etapa 1/4] Gerando os dados brutos e ruidosos em CSV...")
     gerar_dados(caminho_arquivo=ARQUIVO_CSV, quantidade=QTD_REGISTROS)
     print(f" -> Arquivo criado mockado no caminho '{ARQUIVO_CSV}' ({QTD_REGISTROS} registros).")
     
-    # ---------------------------------------------------------
-    # Etapa 2: Ler e Clean/Limpar Dados (O Motor de Limpeza)
-    # ---------------------------------------------------------
+
     print("\n[Etapa 2/4] Lendo CSV e aplicando as regras de validação...")
     dados_limpos = processar_dados_csv(caminho_arquivo=ARQUIVO_CSV)
     print(f" -> Processamento concluído. {len(dados_limpos)} de {QTD_REGISTROS} pacientes passaram na validação.")
     
-    # ---------------------------------------------------------
-    # Etapa 3: Banco de Dados relacional SQLite (A Persistência)
-    # ---------------------------------------------------------
+  
     print("\n[Etapa 3/4] Inicializando banco SQLite local e persistindo...")
     inicializar_banco(nome_banco=BANCO_NOME)
     salvar_dados_lote(dados=dados_limpos, nome_banco=BANCO_NOME)
     print(f" -> Tabela SQLite resetada, INSERT em batch realizado com segurança.")
     
-    # ---------------------------------------------------------
-    # Etapa 4: Analytics
-    # ---------------------------------------------------------
     print("\n[Etapa 4/4] Executando análise em SQL no SQLite...")
     exibir_relatorio_analitico(nome_banco=BANCO_NOME)
     
